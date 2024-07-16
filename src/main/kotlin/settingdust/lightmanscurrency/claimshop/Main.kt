dc1ef8dd34cfbc755daddbaf8ea77a24f8747741
SettingDust <settingdust@gmail.com>
2024-07-16 12:27:47 +0800

    feat: add main class

---
+++ src/main/kotlin/settingdust/lightmanscurrency/claimshop/Main.kt
@@ -0,0 +1,8 @@
+package settingdust.lightmanscurrency.claimshop
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(ClaimShopForLightmansCurrency.ID)
+object ClaimShopForLightmansCurrency {
+    const val ID = "claim_shop_for_lightmans_currency"
+}
